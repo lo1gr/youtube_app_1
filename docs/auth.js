021055afee4054f32df76827b9e4f3c44a9d183e
Lo1gr <louis.gruand@free.fr>
2018-12-30 09:52:34 -0400

    Update docs/auth.js

--- docs/auth.js
+++ docs/auth.js
@@ -2,7 +2,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = "";
+var OAUTH2_CLIENT_ID = "254375087411-fduu7lk4qeqqdvag0136buv6k0hhcrui.apps.googleusercontent.com";
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
